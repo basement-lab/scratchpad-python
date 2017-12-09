c66d45218586f63b8e1f1ed5ff746a6604bd5af8
Derek Clair <derek@derekclair.com>
2017-12-09 01:56:36 -0700

    Linear Regression

---
+++ basic_sigmoid.py
@@ -0,0 +1,23 @@
+
+import math
+
+
+def basic_sigmoid(x):
+    """
+    Compute sigmoid of x.
+
+    Arguments:
+    x -- A scalar
+
+    Return:
+    s -- sigmoid(x)
+    """
+
+    # START CODE HERE ### (≈ 1 line of code)
+    s = 1 / (1 + math.exp(x * -1))
+    ### END CODE HERE ###
+
+    return s
+
+
+print(basic_sigmoid(3))
