890d6381be887043525c6fcbd6f8dab900ba6286
Derek Clair <derek@derekclair.com>
2017-12-21 02:33:22 -0700

    Modulized the Squared Error Function

--- bootstrap.py
+++ bootstrap.py
@@ -2,11 +2,20 @@
 import numpy as np
 
 from postgres import Postgres
+from Squared_Error_Function import cost_func
 
 pg = Postgres()
 
-res = pg.query('SELECT x, y FROM linear_regression;')
-
-print(res)
+ds = pg.query('SELECT x, y FROM linear_regression;')
 
 pg.close()
+
+print('-3', cost_func(ds, -3))
+print('-2', cost_func(ds, -2))
+print('-1', cost_func(ds, -1))
+print('0', cost_func(ds, 0))
+print('1', cost_func(ds, 1))
+print('2', cost_func(ds, 2))
+print('3', cost_func(ds, 3))
+print('4', cost_func(ds, 4))
+
